bf2db47d892e2e1feae89cccbdf3e3262c36aec7
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-08-14 13:40:19 +0530

    Added a typing effect in initial load if converstion is empty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Conversation from "@/components/Chat/Conversation";
 import InputField from "@/components/Chat/InputField";
+import TypingEffectText from "@/components/TypeEffect";
 import DefaultLayout from "@/layouts/Default";
 import { ConversationItem } from "@/types/ChatBotTypes";
 import { useState } from "react";
@@ -13,11 +14,23 @@ export default function Home() {
   return (
     <DefaultLayout>
       <div
-        className="flex flex-col h-full overflow-hidden"
+        className="flex flex-col justify-center h-full overflow-hidden"
       >
-        <Conversation
-          conversation={conversation}
-        />
+        {
+          conversation.length === 0 &&
+          <TypingEffectText
+            text={"AI Art By Me"}
+            className="text-3xl font-black text-center"
+          />
+        }
+
+        {
+          conversation.length > 0 &&
+          <Conversation
+            conversation={conversation}
+          />
+        }
+
         <InputField
           setConversation={setConversation}
         />
